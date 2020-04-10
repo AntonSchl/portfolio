877bfd678e2875089b18e82c03e9f79409afa813
ASGit <anton.schlegel2@gmail.com>
2020-04-10 12:25:37 +0200

    Template literals

--- JavaScript_hadaj_cislo/script2.js
+++ JavaScript_hadaj_cislo/script2.js
@@ -1,21 +1,21 @@
 let correctGuess = false;
 let randomNumber = Math.floor(Math.random() * 6) + 1;
 let guess = prompt("Hádaj na aké číslo medzi 1 až 6 myslím.");
-if(parseInt(guess) === randomNumber){
+if(+(guess) === randomNumber){
     correctGuess = true
-}else if(parseInt(guess) > randomNumber){
-    let guessLess = prompt("Číslo na ktoré myslím je menšie ako " + guess + ".");
-    if (parseInt(guessLess) === randomNumber){
+}else if(+(guess) > randomNumber){
+    let guessLess = prompt(`Číslo na ktoré myslím je menšie ako ${guess}.`);
+    if (+(guessLess) === randomNumber){
         correctGuess = true;
     }
-}else if(parseInt(guess) < randomNumber){
-    let guessMore = prompt("Číslo na ktoré myslím je vačšie ako " + guess + ".");
-    if (parseInt(guessMore) === randomNumber){
+}else if(+(guess) < randomNumber){
+    let guessMore = prompt(`Číslo na ktoré myslím je vačšie ako ${guess}.`);
+    if (+(guessMore) === randomNumber){
         correctGuess = true;
     }
 }
 if (correctGuess){
     document.write("<p>Uhádol si. Gratulujem!!</p>");
 }else {
-    document.write("<p>Žiaľ neuhádol si!! Číslo na ktoré som myslel bolo " + randomNumber + ".</p>");
+    document.write(`<p>Žiaľ neuhádol si!! Číslo na ktoré som myslel bolo ${randomNumber}.</p>`);
 }
